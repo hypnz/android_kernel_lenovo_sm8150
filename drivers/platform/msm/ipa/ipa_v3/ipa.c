bb07082ebeed67d53d31386eeac8b049da9effc2
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-06-24 19:55:12 +0200

    platform: msm: ipa_v3: Fix hard dependency on PCI
    PCI is not necessary for IPA to work: it depends on the location
    of the modem, which isn't necessarily on the PCI/PCIe bus.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -9086,12 +9086,14 @@ void ipa_pc_qmp_enable(void)
 /**************************************************************
  *            PCIe Version
  *************************************************************/
-
 int ipa3_pci_drv_probe(
 	struct pci_dev            *pci_dev,
 	struct ipa_api_controller *api_ctrl,
 	const struct of_device_id *pdrv_match)
 {
+#ifndef CONFIG_PCI
+	return -ENXIO;
+#else
 	int result;
 	struct ipa3_plat_drv_res *ipa_drv_res;
 	u32 bar0_offset;
@@ -9245,6 +9247,7 @@ int ipa3_pci_drv_probe(
 	}
 
 	return result;
+#endif
 }
 
 /*
